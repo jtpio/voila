eeb4098b65acd556202209d34fd24c10ee971695
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-05-22 17:21:07 +0200

    Send X-XSRFToken header

--- share/jupyter/voila/templates/base/static/main.js
+++ share/jupyter/voila/templates/base/static/main.js
@@ -39,9 +39,12 @@ require(['static/voila'], function(voila) {
 
         async function init() {
             // it seems if we attach this to early, it will not be called
+            const matches = document.cookie.match('\\b_xsrf=([^;]*)\\b');
+            const xsrfToken = (matches && matches[1]) || '';
             window.addEventListener('beforeunload', function (e) {
                 const xhttp = new XMLHttpRequest();
                 xhttp.open("DELETE", `/api/kernels/${kernel.id}`, false);
+                xhttp.setRequestHeader('X-XSRFToken', xsrfToken);
                 xhttp.send();
                 kernel.dispose();
             });
